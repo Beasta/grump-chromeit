4aa60ae991dfc1202a797b2e2d8628e0b93434d9
Beasta <barryblaha@gmail.com>
2017-05-01 23:49:53 -0700

    automatically check for and install live-server

--- chromeit.sh
+++ chromeit.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+if which live-server; then
+    echo 'live-server is installed'
+else
+    echo 'live-server is not  installed. installing'
+    npm i -g live-server
+fi
+
 if [ $# -eq 0 ]
   then
-    echo "Give me a filename ... like this 'grump chromit filename.js"
+    echo "Give me a filename ... like this 'grump chromeit filename.js"
 fi
 
 for arg
 do
-  echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body><script src="' $arg '"type="text/javascript"></script></body></html>' > index.html
-
-  live-server 
-
+  echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body><script src="' $arg '"type="text/javascript"></script></body></html>' > _chromeit.html
+  live-server --entry-file="_chromeit.html"
 done
