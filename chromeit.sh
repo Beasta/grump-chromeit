78da06ea95db9f45449b6f28884df27e69348d42
Beasta <barryblaha@gmail.com>
2017-05-16 21:37:46 -0700

    add browserify for bundling

--- chromeit.sh
+++ chromeit.sh
@@ -7,6 +7,13 @@ else
     npm i -g live-server
 fi
 
+if which browserify; then
+    echo 'browserify is installed'
+else
+    echo 'browserify is not  installed. installing'
+    npm i -g browserify
+fi
+
 if [ $# -eq 0 ]
   then
     echo "Give me a filename ... like this 'grump chromeit filename.js"
@@ -14,6 +21,7 @@ fi
 
 for arg
 do
-  echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body><script src="' $arg '"type="text/javascript"></script></body></html>' > _chromeit.html
+  browserify $arg > bundle.js
+  echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body><script src="' bundle.js '"type="text/javascript"></script></body></html>' > _chromeit.html
   live-server --entry-file="_chromeit.html"
 done
